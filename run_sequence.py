93e4d99832a7c44f4a94988ba3e4faa30e10b166
Kun Qiu <kun.qiu@vanderbilt.edu>
2025-12-03 16:52:41 -0500

    Corrected Push Sequence Solver

--- run_sequence.py
+++ run_sequence.py
@@ -22,31 +22,33 @@ HOUGH_DENSITY = 10
 
 def parser_setup():
     parser = argparse.ArgumentParser(
-        description="Hybrid Analysis Method for Multiple Time Frame",
-        epilog="Usage: python run_sequence.py Ref Run 9 11 10 1 1e-6 0.000039604 --num 3 --filter True"
+        description="Hybrid Analysis Method for Single Time Frame",
+        epilog="Usage: python run_sequence.py Ref1.tif 1us.tif 2us.tif 9 11 2e-6 0.000039604 --thresh 10 1 --interp 0 --num 3 --filter True"
     )
 
-    parser.add_argument("ref", required=True, type=str, help="Path to the folder of reference image")
-    parser.add_argument("mov", required=True, type=str, help="Path to the folder of moving image")
-    parser.add_argument("line", nargs=2, required=True, type=int, help="Number of lines")
-    parser.add_argument("slope", nargs=2, required=True, type=int, help="Slope of lines")
-    parser.add_argument("dt", required=True, type=float, help="Delay time between reference and moving image")
-    parser.add_argument("pix_world", required=True, type=float, help="Conversion factor for pixel to world coordinates")
+    parser.add_argument("ref", type=str, help="Path to the reference image")
+    parser.add_argument("mov",  type=str, nargs='+', help="Path to the list moving image")
+    parser.add_argument("line", nargs=2, type=int, help="Number of lines")
+    parser.add_argument("dt", type=float, help="Delay time between reference and moving image")
+    parser.add_argument("pix_world", type=float, help="Conversion factor for pixel to world coordinates")
+    parser.add_argument("--thresh", nargs=2, type=int, help="Slope threshold for Hough transform")
+    parser.add_argument("--interp", type=int, default=0, help="Interpolation method")
     parser.add_argument("--num", default=0, type=int, help="Number of images to process")
     parser.add_argument("--filter", default=False, type=bool, help="Boolean to filter single shot")
     return parser
 
 
-def solver_setup(ref, mov, num_lines, slope_thresh, ref_avg=None, mov_avg=None):
+def solver_setup(ref, mov, num_lines, slope_thresh, interp, ref_avg=None, mov_avg=None):
     """
-    Setup HoughTM solver with given parameters.
-    """
+    Parameter setup for the solver
+    """ 
     solver = HoughTM(
         ref=ref,
         ref_avg=ref_avg,
         mov=mov,
         mov_avg=mov_avg,
         num_lines=num_lines,
+        interp=interp,
         slope_thresh=slope_thresh
     )
 
@@ -71,37 +73,42 @@ if __name__ == "__main__":
 
     print("Pre-Processing reference & moving image...")
     
-    ref_tif_arr, mov_tif_arr = [] * 2
-    ref_avg_arr, mov_avg_arr = None * 2
-    for ref_tif, mov_tif in zip(args.ref, args.mov):
-        ref_tif_arr.append(tifReader(ref_tif)) 
-        mov_tif_arr.append(tifReader(mov_tif))
+    ref_tif = tifReader(args.ref)
+    ref_avg = ref_tif.average() if args.filter else None
 
-        if args.filter != None:
-            ref_avg_arr.append(ref_tif.average()) 
-            mov_avg_arr.append(mov_tif.average())
+    mov_tif_mat, mov_avg_mat = [], []    
+    for mov_tif in args.mov:
+        mov_tif_obj = tifReader(mov_tif)
+        mov_tif_mat.append(mov_tif_obj)
+        mov_avg_mat.append(mov_tif_obj.average() if args.filter != None else None) 
 
-    min_length = min(ref_tif_arr[0].length, mov_tif_arr[0].length)
+    min_length = min(ref_tif.length, mov_tif_mat[0].length)
     num_lines = (args.line[0], args.line[1])
-    slope_thresh = (args.slope[0], args.slope[1])
+    interpolation = args.interp
+    slope_thresh = args.thresh if args.thresh else (10, 1)
     
-    img_shape = np.shape(ref_avg_arr[0])
+    img_shape = np.shape(ref_avg)
     processor = PostProcessor(img_shape[0], img_shape[1])
 
     print("Reference and moving image processed.")
 
-    skip = [5, 6, 10, 11, 45]
+    skip = [5, 6, 10, 11, 14, 45, 47, 68, 69, 71, 72, 74]
     num = (min_length if args.num==0 else args.num)
-
     for i in tqdm(range(num), desc="Processing images"):
-        ref, mov = ref_tif.get_image(i), mov_tif.get_image(i)
+        ref, mov = ref_tif.get_image(i), mov_tif_mat[0].get_image(i)
         solver = solver_setup(
-            ref, mov, num_lines, 
-            slope_thresh, ref_avg_arr[0], mov_avg_arr[0]
+            ref, mov, num_lines, slope_thresh, 
+            interpolation, ref_avg, mov_avg_mat[0]
             )
-        solver.solve()
+        
+        mov_sequence = []
+        for mov_tif in mov_tif_mat[1:]:
+            mov_sequence.append(mov_tif.get_image(i))
 
+        solver.sequence_solver(mov_sequence, mov_avg_mat[1:])
+        solver.solve()
         if (i + 1) in skip:
+            print(f"Skipping frame {i + 1} due to known issues.")
             continue            
 
         sol_field = solver.get_fields(dt=args.dt, pix_to_world=args.pix_world)
@@ -122,44 +129,51 @@ if __name__ == "__main__":
     Vy = vy_mean[Y, X]
 
     Vel_mag = vel_mean_mag[Y, X]
-    unit_Vx = Vx / Vel_mag
-    unit_Vy = Vy / Vel_mag
+    Vel_max_mag = np.nanmax(Vel_mag) if len(Vel_mag) > 0 else 1.0
+    unit_Vx = Vx / Vel_max_mag
+    unit_Vy = -Vy / Vel_max_mag
 
     # Velocity Magnitude Plot
     fig, ax = plt.subplots(figsize=(8, 6))
     mag_plot = ax.imshow(vel_mean_mag, cmap='RdBu_r', origin='upper')
-    ax.quiver(X, Y, unit_Vx, unit_Vy, color='red', scale=20)
-    cbar = fig.colorbar(mag_plot, ax=ax, format='%.1e')
+    ax.quiver(X, Y, unit_Vx, unit_Vy, color='black', scale=20)
+    cbar = fig.colorbar(mag_plot, ax=ax, format='%.1e', )
     ax.set_title("Magnitude (m/s)")
 
 
-    fig, axs = plt.subplots(2, 3, figsize=(18, 10))
+    fig_avg, axs_avg = plt.subplots(1, 3, figsize=(18, 6))
+    # Mean U with vectors
+    im0 = axs_avg[0].imshow(vx_mean, cmap='jet')
+    axs_avg[0].set_title('Mean U (m/s)')
+    plt.colorbar(im0, ax=axs_avg[0], shrink=0.5)
 
-    # Mean Velocity Plots
-    im0 = axs[0, 0].imshow(vx_mean, cmap='jet')
-    axs[0, 0].set_title('Mean U (m/s)')
-    plt.colorbar(im0, ax=axs[0, 0])
+    # Mean V with vectors
+    im1 = axs_avg[1].imshow(vy_mean, cmap='jet')
+    axs_avg[1].set_title('Mean V (m/s)')
+    plt.colorbar(im1, ax=axs_avg[1], shrink=0.5)
 
-    im1 = axs[0, 1].imshow(vy_mean, cmap='jet')
-    axs[0, 1].set_title('Mean V (m/s)')
-    plt.colorbar(im1, ax=axs[0, 1])
+    # Mean Vorticity with vectors
+    im2 = axs_avg[2].imshow(vort_mean, cmap='jet')
+    axs_avg[2].set_title('Mean ω (1/s)')
+    plt.colorbar(im2, ax=axs_avg[2], shrink=0.5)
 
-    im2 = axs[0, 2].imshow(vort_mean, cmap='jet')
-    axs[0, 2].set_title('Mean ω​​ (1/s)')
-    plt.colorbar(im2, ax=axs[0, 2])
+    plt.tight_layout()
 
-    # RMS Velocity Plots
-    im3 = axs[1, 0].imshow(vx_std, cmap='jet')
-    axs[1, 0].set_title('U\' (m/s)')
-    plt.colorbar(im3, ax=axs[1, 0])
+    fig_rms, axs_rms = plt.subplots(1, 3, figsize=(18, 6))
+    # RMS U
+    im3 = axs_rms[0].imshow(vx_std, cmap='jet')
+    axs_rms[0].set_title('U\' (m/s)')
+    plt.colorbar(im3, ax=axs_rms[0], shrink=0.5)
 
-    im4 = axs[1, 1].imshow(vy_std, cmap='jet')
-    axs[1, 1].set_title('V\' (m/s)')
-    plt.colorbar(im4, ax=axs[1, 1])
+    # RMS V
+    im4 = axs_rms[1].imshow(vy_std, cmap='jet')
+    axs_rms[1].set_title('V\' (m/s)')
+    plt.colorbar(im4, ax=axs_rms[1], shrink=0.5)
 
-    im5 = axs[1, 2].imshow(vort_std, cmap='jet')
-    axs[1, 2].set_title('ω​​\' (1/s)')
-    plt.colorbar(im5, ax=axs[1, 2])
+    # RMS Vorticity
+    im5 = axs_rms[2].imshow(vort_std, cmap='jet')
+    axs_rms[2].set_title('ω\' (1/s)')
+    plt.colorbar(im5, ax=axs_rms[2], shrink=0.5)
 
     plt.tight_layout()
     plt.show()
